aa8584266e456dd1ccb3ea2226323cda5d317184
bill <hszhongtao@163.com>
2020-07-14 10:40:16 +0800

    部署脚本bug fix

--- sh/kunkka.sh
+++ sh/kunkka.sh
@@ -14,9 +14,9 @@ mvn package -Dmaven.test.skip=true
 cp /home/code/kunkka/target/kunkka-1.0.0.jar /home/server
 
 # 将上次启动的kunkka实例杀掉
-for kunkka in `ps -ef | grep kunkka | grep java | grep -v grep | awk '{print $2}'`;do
-	echo 'killing $kunkka'
-	kill -9 $kunkka
+for pid in `ps -ef | grep kunkka | grep java | grep -v grep | awk '{print $2}'`;do
+	echo 'killing '$pid
+	kill -9 $pid
 done
 
 # 启动两个实例
